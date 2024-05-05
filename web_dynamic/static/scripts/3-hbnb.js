ca448c603ffcbde78cbf0f769e0079d5b93b967c
osamanazar47 <osamanazar47@gmail.com>
2024-05-05 22:59:49 +0300

    all

---
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -0,0 +1,46 @@
+document.ready(function () {
+	const amenities = {};
+	$('[data-id]').css('marginLeft', '30px');
+	$("li input[type=checkbox]").change(function () {
+		if (this.checked) {
+			amenities[this.dataset.name] = this.dataset.id;
+		} else {
+			delete amenities[this.dataset.name];
+		}
+		$(".amenities h4").text(Object.keys(amenities).sort().join(", "));
+	});
+
+	$.getJSON('http://0.0.0.0:5001/api/v1/status/', (data) => {
+		if (data.status === 'OK') {
+			$("div#api_status").addClass("available");
+		} else {
+			$("div#api_status").removeClass("available");
+		}
+	});
+
+	$.ajax({
+		type: 'POST',
+		url: 'http://localhost:5001/api/v1/places_search',
+		data: '{}',
+		dataType: 'json',
+		contentType: 'application/json',
+		success: function (data) {
+		  $('SECTION.places').append(data.map(place => {
+			return `<article>
+					  <div class="title_box">
+						<h2>${place.name}</h2>
+						<div class="price_by_night">${place.price_by_night}</div>
+					  </div>
+					  <div class="information">
+						<div class="max_guest">${place.max_guest} Guests</div>
+						<div class="number_rooms">${place.number_rooms} Bedrooms</div>
+						<div class="number_bathrooms">${place.number_bathrooms} Bathrooms</div>
+					  </div>
+					  <div class="description">
+						${place.description}
+					  </div>
+					</article>`
+		  }));
+		}
+	});
+});
