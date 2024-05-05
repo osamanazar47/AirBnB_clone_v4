d778cbe31457e0ba2514c5e4acdaae2e73331673
osamanazar47 <osamanazar47@gmail.com>
2024-05-05 21:26:40 +0300

    2-hbnb

---
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -0,0 +1,12 @@
+document.ready(function () {
+	const amenities = {};
+	$('[data-id]').css('marginLeft', '30px');
+	$("li input[type=checkbox]").change(function () {
+		if (this.checked) {
+			amenities[this.dataset.name] = this.dataset.id;
+		} else {
+			delete amenities[this.dataset.name];
+		}
+		$(".amenities h4").text(Object.keys(amenities).sort().join(", "));
+	});
+})
